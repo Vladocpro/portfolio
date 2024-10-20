28b18e8ad099ec7247e7687eb214561f4fa181cd
vladocpro <infernohs9@gmail.com>
2024-10-20 23:19:46 +0100

    Updated link icon in projects section

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -43,7 +43,7 @@ const Projects = () => {
                    <span className="font-medium text-sm sm:text-base text-justify">{projectData.description}</span>
                 </div>
                 <div className="flex justify-between items-center">
-                   <div className={`flex gap-0.5 ${projectData.demoAccount ? "mt-0" : "mt-3"}`}>
+                   <div className={`flex gap-1 ${projectData.demoAccount ? "mt-0" : "mt-3"}`}>
                    <a href={projectData.github} rel="noreferrer noopener" target="_blank">
                          <svg xmlns="http://www.w3.org/2000/svg"
                               className={`w-[35px] sm:w-[40px] cursor-pointer stroke-white transition-all duration-200 hover:fill-gray-500 hover:scale-110`}
@@ -66,13 +66,31 @@ const Projects = () => {
                                 {/*   <path d="M11 6l.463 -.536a5 5 0 0 1 7.071 7.072l-.534 .464"/>*/}
                                 {/*   <path d="M13 18l-.397 .534a5.068 5.068 0 0 1 -7.127 0a4.972 4.972 0 0 1 0 -7.071l.524 -.463"/>*/}
                                 {/*</svg>*/}
-                                <svg xmlns="http://www.w3.org/2000/svg" className={`w-[35px] sm:w-[40px] cursor-pointer stroke-white transition-all duration-200 hover:stroke-gray-500 hover:scale-110`}
-                                     viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                                   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                                   <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"/>
-                                   <path d="M11 13l9 -9"/>
-                                   <path d="M15 4h5v5"/>
+                                {/*<svg xmlns="http://www.w3.org/2000/svg"*/}
+                                {/*     className={`w-[35px] sm:w-[40px] cursor-pointer stroke-white transition-all duration-200 hover:stroke-gray-500 hover:scale-110`}*/}
+                                {/*     viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" fill="none" strokeLinecap="round" strokeLinejoin="round">*/}
+                                {/*   <path stroke="none" d="M0 0h24v24H0z" fill="none"/>*/}
+                                {/*   <path d="M12 6h-6a2 2 0 0 0 -2 2v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-6"/>*/}
+                                {/*   <path d="M11 13l9 -9"/>*/}
+                                {/*   <path d="M15 4h5v5"/>*/}
+                                {/*</svg>*/}
+                                <svg xmlns="http://www.w3.org/2000/svg"
+                                     className={`w-[30px] sm:w-[35px] mt-1 sm:mt-0.5 cursor-pointer stroke-white transition-all duration-200 hover:stroke-gray-500 hover:scale-110`}
+                                     fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"
+                                >
+                                   <path strokeLinecap="round" strokeLinejoin="round"
+                                         d="M13.19 8.688a4.5 4.5 0 0 1 1.242 7.244l-4.5 4.5a4.5 4.5 0 0 1-6.364-6.364l1.757-1.757m13.35-.622 1.757-1.757a4.5 4.5 0 0 0-6.364-6.364l-4.5 4.5a4.5 4.5 0 0 0 1.242 7.244"/>
                                 </svg>
+                                {/*<svg xmlns="http://www.w3.org/2000/svg"*/}
+                                {/*     className={`w-[35px] sm:w-[35px] sm:mt-0.5 cursor-pointer stroke-white transition-all duration-200 hover:stroke-gray-500 hover:scale-110`}*/}
+
+                                {/*     fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor"*/}
+                                {/*     >*/}
+                                {/*   <path strokeLinecap="round" strokeLinejoin="round"*/}
+                                {/*         d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418"/>*/}
+                                {/*</svg>*/}
+
+
                              </a>
                          ) : null
                       }
